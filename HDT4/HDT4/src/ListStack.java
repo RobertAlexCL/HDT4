a85c69a0795c4f3a8e7de64d7a8af25e4690f8ad
RobertAlexCL <46799325+RobertAlexCL@users.noreply.github.com>
2019-02-15 18:33:00 -0600

    Cambios

--- HDT4/HDT4/src/ListStack.java
+++ HDT4/HDT4/src/ListStack.java
@@ -1,143 +1,38 @@
 
-import java.util.Collection;
-import java.util.Iterator;
+import java.awt.*;
 import java.util.List;
-import java.util.ListIterator;
+import java.util.Stack;
 
 public class ListStack<E> extends AbstractStack<E> {
-    private List<E> data;
+    private AbstractList<E> list;
+    private FactoryList<E> factory = new FactoryList<>();
+    private String listType;
 
-    public ListStack() {
-        data = new List<E>() {
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            @Override
-            public Iterator<E> iterator() {
-                return null;
-            }
-
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @Override
-            public <T> T[] toArray(T[] a) {
-                return null;
-            }
-
-            @Override
-            public boolean add(E e) {
-                return false;
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(Collection<? extends E> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(int index, Collection<? extends E> c) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public E get(int index) {
-                return null;
-            }
-
-            @Override
-            public E set(int index, E element) {
-                return null;
-            }
-
-            @Override
-            public void add(int index, E element) {
-
-            }
-
-            @Override
-            public E remove(int index) {
-                return null;
-            }
-
-            @Override
-            public int indexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public int lastIndexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public ListIterator<E> listIterator() {
-                return null;
-            }
-
-            @Override
-            public ListIterator<E> listIterator(int index) {
-                return null;
-            }
-
-            @Override
-            public List<E> subList(int fromIndex, int toIndex) {
-                return null;
-            }
-        };
-    }
+    public ListStack(String listType) {
+        this.listType = listType;
+       list = factory.SetType(listType);
+       }
 
     @Override
     public void push(E item) {
-        data.add(item);
+
+        list.addFirst(item);
     }
 
     @Override
     public E pop() {
-        return data.remove(size() -1);
+
+
+        if(listType.equals("DL")){
+            return list.removeLast();
+        }
+        return list.removeFirst();
+
     }
 
     @Override
     public int size() {
-        return data.size();
+        return list.size();
+
     }
 }
