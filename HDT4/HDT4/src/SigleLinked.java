0ddfd5f63e22c6f654e69340ac90375bc4285272
RobertAlexCL <46799325+RobertAlexCL@users.noreply.github.com>
2019-02-15 18:52:03 -0600

    Cambios

---
+++ HDT4/HDT4/src/SigleLinked.java
@@ -0,0 +1,10 @@
+
+public class SigleLinked<E> {
+    public SigleLinked nodo;
+    E valor;
+
+    public SigleLinked(SigleLinked nodo, E valor) {
+        this.nodo = nodo;
+        this.valor = valor;
+    }
+}
