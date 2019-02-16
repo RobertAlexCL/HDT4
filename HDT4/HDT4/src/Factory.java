ebe35eff1862de8f16d5a6211778728f9fbb4b69
RobertAlexCL <46799325+RobertAlexCL@users.noreply.github.com>
2019-02-15 23:53:04 -0600

    Cambios

--- HDT4/HDT4/src/Factory.java
+++ HDT4/HDT4/src/Factory.java
@@ -1,18 +1,13 @@
 
-public class Factory {
+public class Factory<E> {
 
     public AbstractStack SetType(String type){
         VectorStack vector = new VectorStack();
         if (type.equals("Vector")) {
-            return new VectorStack<String>();
+            return new VectorStack<E>();
         } else if(type.equals("AL")){
-            return new ArrayListStack<String>();
-        } else if(type.equals("SList")){
-            return new SingleLinkedList<String>();
-        }else if (type.equals("DList")){
-            return new DoubleLinkedList<String>();
-        } else if (type.equals("CList")){
-            return new CircularLinkedList<String>();
-        } else return null;
-    }
-}
+
+            return new ArrayListStack<E>();
+        }
+            return null;
+        }}
