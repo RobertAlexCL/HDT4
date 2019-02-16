500ede74b996d425e7f971f871b0069b7fd398e3
RobertAlexCL <46799325+RobertAlexCL@users.noreply.github.com>
2019-02-15 19:52:03 -0600

    Cambios

--- HDT4/HDT4/src/icalculator.java
+++ HDT4/HDT4/src/icalculator.java
@@ -1,7 +1,7 @@
 
 public interface icalculator {
-    public double sumar(double num1, double num2); 
-    public double restar(double num1, double num2); 
-    public double multiplicacion(double num1, double num2); 
-    public double division(double num1, double num2);
+    public double sumar(double num1, double num2); // Se suman
+    public double restar(double num1, double num2); // Se restan
+    public double multiplicacion(double num1, double num2); // Se multiplican
+    public double division(double num1, double num2); // Se dividen
 }
